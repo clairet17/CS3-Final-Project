cdfdc483bb681cd83421048f835f41a073029ff7
Claire Trop <ctrop17@gmail.com>
2015-05-11 19:05:05 -0700

    Version 4

--- ScoreCard.java
+++ ScoreCard.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class ScoreCard
 {
    private Object[][] scoreTracker;
@@ -41,7 +42,84 @@ public class ScoreCard
    
    public int calcScore(String scoringLoc, Hand hand, int location)
    {
-      return 0;  
+      int value=0;
+      if(scoreTracker[location][2]==null || scoringLoc.equals("YAHTZEE"))
+      {
+         if(scoringLoc.equals("Chance"))
+         {
+            value=hand.totalHand();
+            return value;
+         }
+         if(location>=0 && location<=5)
+         {
+            value=hand.addUp(location+1);
+         }
+         else
+         {
+            if(scoringLoc.equals("3 of a kind"))
+            {
+               for(int three=1; three<7; three++)
+               {
+                  int temp=hand.addUp(three);
+                  if(temp>=3)
+                  {
+                     value=hand.totalHand();
+                  }
+               }
+            }
+            if(scoringLoc.equals("4 of a kind"))
+            {
+               for(int four=1; four<7; four++)
+               {
+                  int temp=hand.addUp(four);
+                  if(temp>=4)
+                  {
+                     value=hand.totalHand();
+                  }
+               }
+            }
+            if(scoringLoc.equals("Full House"))//maybe do a good ole ignore case to make more user friendly
+            {
+               boolean has2=false;
+               boolean has3=false;
+               for(int FL=1; FL<7; FL++)
+               {
+                  int temp=hand.addUp(FL);
+                  if(temp==3)
+                     has3=true;
+                  if(temp==2)
+                     has2=true;
+               }
+               if(has2==true && has3==true)
+                  value=25;
+            }
+            if(scoringLoc.equals("YAHTZEE"))
+            {
+               for(int Y=1; Y<7; Y++)//possible to eliminate redundancy with refactoring?
+               {
+                  int temp=hand.addUp(Y);
+                  if(temp==5)
+                  {
+                     numYahtzees++;
+                     value=50*numYahtzees;
+                  }
+               }
+            }
+            
+            if(scoringLoc.equals("Sm Straight"))
+            {
+               int[] temp=new int[5];
+               for(int set=0; set<6; set++)
+               {
+                  if(hand.addUp(set+1)<=1)//removes chance of doubles
+                  {
+                     temp[set]=hand.getValueOfDie(set);
+                  }
+               }
+               Arrays.sort(temp);
+            }
+         }
+      }
    }
    
    private int findString(String scoringLoc)
